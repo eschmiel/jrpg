86122d4a69f62005698bde83c82472a93a66e8db
Eric Schmiel <eschmiel@gmail.com>
2026-02-27 17:54:55 -0500

    debug control and draw

--- game/game_state.lua
+++ game/game_state.lua
@@ -7,29 +7,12 @@ function mk_game_state()
         update = function(self)
             self.controller:run(self)
             handle_block_collisions(self)
-            if(btnp(INPUT.X)) then
-                log('zone change')
-                logTbl(self.zone.map)
-                if(self.zone == zone1) then
-                    log('eep')
-                    self.zone = zone2
-                    self.player.x += zone2.map[1]*8
-                    self.player.y += zone2.map[2]*8
-                    
-                
-                elseif(self.zone == zone2) then
-                    log('bee')
-                    self.zone = zone1
-                    self.player.x -= zone2.map[1]*8
-                    self.player.y -= zone2.map[2]*8
-                    
-                end
-            end
+            debug_controller(self)
         end,
 
         draw = function(self)
             
-            camera(self.zone.map[1]*8, self.zone.map[2]*8)
+            camera(self.zone.map[1]*8 , self.zone.map[2]*8)
             -- camera(self.player.x-50,self.player.y-70)
             render_zone(self.zone)
             render_player(self.player)
@@ -37,8 +20,7 @@ function mk_game_state()
             if(self.loaded_dialogue) then
                 render_dialogue(self.loaded_dialogue)
             end
-            if(self.zone == zone1) print('zone 1',2,2,COLOR.BLACK)
-            if(self.zone == zone2) print('zone 2',2,2,COLOR.BLACK)
+            debug_draw(self)
             
         end
     }
@@ -54,4 +36,29 @@ end
 
 function m_spr(gid,x,y)
     spr(gid,x*8,y*8)
+end
+
+function debug_controller(state)
+    if(btnp(INPUT.X)) then
+        log('zone change')
+        logTbl(state.zone.map)
+        if(state.zone == zone1) then
+            log('eep')
+            state.zone = zone2
+            state.player.x += zone2.map[1]*8
+            state.player.y += zone2.map[2]*8
+            
+        
+        elseif(state.zone == zone2) then
+            log('bee')
+            state.zone = zone1
+            state.player.x -= zone2.map[1]*8
+            state.player.y -= zone2.map[2]*8
+        end
+    end
+end
+
+function debug_draw(state)
+    if(state.zone == zone1) print('zone 1',2,2,COLOR.BLACK)
+    if(state.zone == zone2) print('zone 2',2,2,COLOR.BLACK)
 end
